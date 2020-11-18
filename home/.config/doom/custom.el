d9fc43f1a46c862456506a6b9eb439296133b400
Bill Wanjohi <bill.wanjohi@civicactions.com>
2020-11-18 18:06:42 +0000

    add civisactions remote type

--- home/.config/doom/custom.el
+++ home/.config/doom/custom.el
@@ -3,6 +3,16 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(browse-at-remote-remote-type-domains
+   '(("bitbucket.org" . "bitbucket")
+     ("github.com" . "github")
+     ("gitlab.com" . "gitlab")
+     ("git.savannah.gnu.org" . "gnu")
+     ("gist.github.com" . "gist")
+     ("git.sr.ht" . "sourcehut")
+     ("pagure.io" . "pagure")
+     ("src.fedoraproject.org" . "pagure")
+     ("git.civicactions.net" . "gitlab")))
  '(org-agenda-custom-commands
    '(("n" "Agenda and all TODOs"
       ((agenda ""
