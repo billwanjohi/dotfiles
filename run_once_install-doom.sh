14d8f6343fcd1922fb26ffc874832690fe935c96
Bill Kaguru Wanjohi <BillWanjohi@RocketAuto.com>
2022-03-04 17:52:10 +0000

    some additional necessary packages

--- run_once_install-doom.sh
+++ run_once_install-doom.sh
@@ -4,6 +4,7 @@ set -Eeuxo pipefail
 
 sudo chown "$(whoami)":admin /usr/local/share/zsh/site-functions
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew install homebrew/cask-fonts/font-space-mono-nerd-font rg
 brew install --cask --no-quarantine emacs
 if [[ ! -d ~/.emacs.d ]]
 then
--- run_once_ssh-install-defaults.sh
+++ run_once_ssh-install-defaults.sh
@@ -4,4 +4,9 @@ set -Eeuxo pipefail
 
 sudo chown "$(whoami)":admin /usr/local/share/zsh /usr/local/share/zsh/site-functions
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-brew install lsd neovim zinit zoxide
+brew install \
+    git-delta \
+    lsd \
+    neovim \
+    zinit \
+    zoxide
