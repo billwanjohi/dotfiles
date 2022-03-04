809e7566695131fce81696ac1dd2c2f97cca0531
Bill Kaguru Wanjohi <BillWanjohi@RocketAuto.com>
2022-03-04 12:12:55 -0500

    first unprivileged commit

---
+++ run_once_ssh-install-defaults.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+set -Eeuxo pipefail
+
+sudo chown "$(whoami)":admin /usr/local/share/zsh /usr/local/share/zsh/site-functions
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew install lsd neovim zinit zoxide
