6515d6a94a7a185ee29ee265a2eb59a493839090
Bill Kaguru Wanjohi <bill@ka.guru>
2022-06-16 22:28:39 +0000

    delete meta now outdated or tracked elsewhere

--- meta/install_zsh.sh
+++
@@ -1,5 +0,0 @@
-brew install zsh
-grep -q $(which zsh) /etc/shells ||            # if brew-zsh not in whitelist
-  echo $(which zsh) | sudo tee -a /etc/shells  #   add it
-echo $SHELL | grep $(which zsh) ||             # if brew-zsh not default
-  chsh -s $(which zsh)                         #   set it
